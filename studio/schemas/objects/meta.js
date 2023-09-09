82875152d62aac3a05f2c66afc5bc8e087efa848
timonforrer <timon.forrer@gmail.com>
2023-09-09 16:35:04 +0200

    Add meta description

--- studio/schemas/objects/meta.js
+++ studio/schemas/objects/meta.js
@@ -9,6 +9,11 @@ export default {
       type: 'string'
     },
     {
+      name: 'description',
+      title: 'Description',
+      type: 'text'
+    },
+    {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
